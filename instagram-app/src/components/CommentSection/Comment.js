bd8e35b3fc727492b3fa88e85b909cf9b91872d5
Alfonso Ruiz <1800ruiz@gmail.com>
2019-05-27 16:25:33 -0700

    Added proptype check for comment and comment input components

--- instagram-app/src/components/CommentSection/Comment.js
+++ instagram-app/src/components/CommentSection/Comment.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Comment = props => {
     return (
@@ -6,6 +7,14 @@ const Comment = props => {
             <p><span>{props.comment.username}</span>{props.comment.text}</p>
         </div>
     );
+}
+
+Comment.propTypes = {
+    comment: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        username: PropTypes.string.isRequired,
+        text: PropTypes.string
+    })).isRequired
 }
 
 export default Comment;
--- instagram-app/src/components/CommentSection/CommentInput.js
+++ instagram-app/src/components/CommentSection/CommentInput.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 const CommentInput = props => {
     return (
@@ -12,6 +13,10 @@ const CommentInput = props => {
             />
         </form>
     )
+}
+
+CommentInput.propTypes = {
+    comment: PropTypes.string.isRequired
 }
 
 export default CommentInput
